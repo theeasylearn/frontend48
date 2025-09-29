37241c9d385b251544d284a57b7f27d42b76445a
Drashti-Mehta-13 <drashtimehta08@gmail.com>
2025-09-29 15:44:13 +0530

    Add files via upload

---
+++ nodejs/lib/site_detail.js
@@ -0,0 +1,4 @@
+module.exports.center='The Easy Learn Acadamy';
+module.exports.address='105,eva surbhi,opp aksharwadi,waghawadi road';
+module.exports.city='Bhavnagr';
+module.exports.contactno=4759283908;
