d3d83ac0dec5ccb88f1598d871305b4085196ec8
Drashti-Mehta-13 <drashtimehta08@gmail.com>
2025-10-07 16:55:41 +0530

    Add files via upload

---
+++ nodejs/lib/bus.js
@@ -0,0 +1,41 @@
+module.exports = [
+    { "from": "bhavnagar", "to": "surat", "type": "ac", "time": "9:00 am", "price": 426 },
+    { "from": "bhavnagar", "to": "surat", "type": "sleeper", "time": "1:30 pm", "price": 178 },
+    { "from": "bhavnagar", "to": "surat", "type": "non-ac", "time": "6:00 pm", "price": 355 },
+
+    { "from": "ahmedabad", "to": "vadodara", "type": "sleeper", "time": "10:30 pm", "price": 489 },
+    { "from": "ahmedabad", "to": "vadodara", "type": "ac", "time": "7:00 am", "price": 215 },
+    { "from": "ahmedabad", "to": "vadodara", "type": "non-ac", "time": "2:00 pm", "price": 344 },
+
+    { "from": "rajkot", "to": "jamnagar", "type": "ac", "time": "6:15 am", "price": 159 },
+    { "from": "rajkot", "to": "jamnagar", "type": "sleeper", "time": "11:00 am", "price": 278 },
+    { "from": "rajkot", "to": "jamnagar", "type": "non-ac", "time": "5:30 pm", "price": 382 },
+
+    { "from": "surat", "to": "mumbai", "type": "non-ac", "time": "3:45 pm", "price": 316 },
+    { "from": "surat", "to": "mumbai", "type": "sleeper", "time": "9:15 am", "price": 475 },
+    { "from": "surat", "to": "mumbai", "type": "ac", "time": "7:45 pm", "price": 238 },
+
+    { "from": "bhavnagar", "to": "ahmedabad", "type": "ac", "time": "7:20 am", "price": 194 },
+    { "from": "bhavnagar", "to": "ahmedabad", "type": "sleeper", "time": "1:50 pm", "price": 128 },
+    { "from": "bhavnagar", "to": "ahmedabad", "type": "non-ac", "time": "8:10 pm", "price": 419 },
+
+    { "from": "vadodara", "to": "rajkot", "type": "sleeper", "time": "11:00 pm", "price": 394 },
+    { "from": "vadodara", "to": "rajkot", "type": "ac", "time": "6:45 am", "price": 183 },
+    { "from": "vadodara", "to": "rajkot", "type": "non-ac", "time": "3:30 pm", "price": 307 },
+
+    { "from": "jamnagar", "to": "bhavnagar", "type": "non-ac", "time": "8:30 am", "price": 401 },
+    { "from": "jamnagar", "to": "bhavnagar", "type": "sleeper", "time": "12:15 pm", "price": 148 },
+    { "from": "jamnagar", "to": "bhavnagar", "type": "ac", "time": "9:45 pm", "price": 361 },
+
+    { "from": "surat", "to": "ahmedabad", "type": "ac", "time": "5:00 pm", "price": 223 },
+    { "from": "surat", "to": "ahmedabad", "type": "non-ac", "time": "10:30 am", "price": 284 },
+    { "from": "surat", "to": "ahmedabad", "type": "sleeper", "time": "11:55 pm", "price": 419 },
+
+    { "from": "mumbai", "to": "vadodara", "type": "sleeper", "time": "2:15 am", "price": 308 },
+    { "from": "mumbai", "to": "vadodara", "type": "ac", "time": "9:00 am", "price": 117 },
+    { "from": "mumbai", "to": "vadodara", "type": "non-ac", "time": "6:30 pm", "price": 396 },
+
+    { "from": "ahmedabad", "to": "rajkot", "type": "ac", "time": "4:45 pm", "price": 347 },
+    { "from": "ahmedabad", "to": "rajkot", "type": "non-ac", "time": "9:30 am", "price": 412 },
+    { "from": "ahmedabad", "to": "rajkot", "type": "sleeper", "time": "10:00 pm", "price": 292 }
+];
