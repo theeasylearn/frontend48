1a21527fb7b5a8e5b21d68b932007f901f1cf7b5
Drashti-Mehta-13 <drashtimehta08@gmail.com>
2025-11-13 10:41:03 +0530

    Add files via upload

---
+++ nodejs/database/api.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var connection = require("./connection");
+//import contact.js 
+var contact = require('./contact');
+var app = express();
+//define middleware 
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+const ROUTE = "/contact";
+
+//use Post Method to insert new contact 
+//input : email,mobile,name,detail
+//all input is required
+app.post(ROUTE, (request,response) => contact.insert(request,response));
+
+//http://localhost:5000/contact
+//fetch all contacts 
+
+// fetch all contact whose email is abc
+//http://localhost:5000/contact?email=abc
+
+//http://localhost:5000/contact?id=1
+// fetch all contact whose id = 1
+app.get(ROUTE,(request,response) => contact.select(request,response));
+
+//delete specific contact 
+//input : email (required)
+app.delete(ROUTE,(request,response) => contact.remove(request,response));
+
+//update specific contact
+//input : email, mobile, name, detail
+app.put(ROUTE, (request,response) => contact.update(request,response));
+app.listen(5000);
+console.log('ready to accept request');
--- nodejs/database/contact.js
+++ nodejs/database/contact.js
@@ -1,31 +1,108 @@
-const connection=require('./connect');
-const express=require('express');
-const app=express();
-
-
-app.get(
-    '/contact',function insert(req,res){
-
-    let email=req.body.email;
-    let name=req.body.name;
-    let mobile=req.body.mobile;
-    let detail=req.body.detail;
-
-    if(email===undefined || name===undefined||mobile===undefined||detail===undefined){
-        res.send("Data is incomplete");
+var connection = require("./connection");
+function insert(request, response) {
+    var email = request.body.email;
+    var mobile = request.body.mobile;
+    var name = request.body.name;
+    var detail = request.body.detail;
+    // input validation 
+    if (email == undefined || mobile === undefined || name === undefined || detail === undefined) {
+        response.json([{ 'error': 'input is missing' }]);
     }
-
-    let sql=`insert into contact(Email,Name,Mobile,Detail) values(${email},${name},${mobile},${detail})`;
-    connection.conn.query(sql,(error)=>{
-        if(error){
-            console.log(error);
+    else {
+        var sql = `INSERT INTO contact(name, email, mobile, detail) VALUES (?,?,?,?)`;
+        var data = [name, email, mobile, detail];
+        //var sql = `INSERT INTO contact(name, email, mobile, detail) VALUES ('${name}','${email}','${mobile}','${detail}')`;
+        connection.con.query(sql, data, function (error, result) {
+            if (error) {
+                response.json([{ 'error': 'error occured' }]);
+            }
+            else {
+                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'contact inserted....' }]);
+            }
+        });
+    }
+    // console.log(email,mobile,name,detail);
+}
+ function select (request, response) {
+    //object destructring
+    let { id, email } = request.query;
+    console.log(id, email);
+    let sql;
+    if (id === undefined && email === undefined) {
+        sql = "select * from contact order by id desc";
+        data = [];
+    }
+    else if (id !== undefined) {
+        sql = "select * from contact where id=?";
+        data = [id];
+    }
+    else if (email !== undefined) {
+        sql = "select * from contact where email=?";
+        data = [email];
+    }
+    connection.con.query(sql, data, function (error, result) {
+        if (error) {
+            response.json([{ 'error': 'error occured' }]);
         }
-        else{
-            console.log("contact inserted");
+        else {
+            if (result.length === 0) {
+                response.json([{ 'error': 'no' }, { 'total': 0 }]);
+            }
+            else {
+                response.json(result);
+            }
         }
-    });
+    })
 }
 
-);
+function del (request, response) {
+    var email = request.body.email;
+    if (email === undefined) {
+        response.json([{ 'error': 'input is missing' }]);
+    }
+    else {
+        var sql = "delete from contact where email=?";
+        var data = [email];
+        connection.con.query(sql, data, function (error, result) {
+            if (error) {
+                response.json([{ 'error': 'error occured' }]);
+            }
+            else {
+                if (result.affectedRows > 0)
+                    response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'contact deleted....' }]);
+                else
+                    response.json([{ 'error': 'no' }, { 'success': 'no' }, { 'message': 'contact not found' }]);
+            }
+        })
+    }
 
-app.listen(5000);
+}
+function update (request, response) {
+    var email = request.body.email;
+    var mobile = request.body.mobile;
+    var name = request.body.name;
+    var detail = request.body.detail;
+    // input validation 
+    if (email == undefined || mobile === undefined || name === undefined || detail === undefined) {
+        response.json([{ 'error': 'input is missing' }]);
+    }
+    else {
+        var sql = "UPDATE contact SET name=?,mobile=?,detail=? WHERE email=?";
+        var data = [name, mobile, detail, email];
+        connection.con.query(sql, data, function (error, result) {
+            if (error) {
+                response.json([{ 'error': 'error occured' }]);
+            }
+            else {
+                if (result.affectedRows > 0)
+                    response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'contact updated....' }]);
+                else
+                    response.json([{ 'error': 'no' }, { 'success': 'no' }, { 'message': 'contact not found' }]);
+            }
+        });
+    }
+}
+module.exports.insert = insert;
+module.exports.select = select;
+module.exports.remove = del;
+module.exports.update = update;
