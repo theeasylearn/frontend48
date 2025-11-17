9ae042073d97e176f68f767b0ccaebb7222e1cfa
theeasylearn <theeasylearn@gmail.com>
2025-11-17 15:47:08 +0530

    hashing done

--- nodejs/lib/security.js
+++ nodejs/lib/security.js
@@ -1,17 +1,24 @@
+const argon2 = require('argon2');
 let security = {
-
-    getHashPassword: (password) => {
-
-        let hashPassword = 'sd45@j6*j7';
-        return hashPassword;
+    getHashPassword: async (password) => {
+        try {
+            const hash = await argon2.hash(password);
+            return hash;
+        }
+        catch (err) {
+            console.error("Hashing failed:", err);
+        }
     },
-
-    comparePassword: (password, hashPassword) => {
-
-        return true;
-
+    comparePassword: async (password, hashPassword) => {
+        try {
+            if (await argon2.verify(hashPassword, password)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err) {
+            console.error("Hashing failed:", err);
+        }
     }
-
-};
-
-module.exports=security;
+}
+module.exports = security;
