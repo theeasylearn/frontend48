6c09ffc5d56081b83d7cf18ea93de069d1e61278
theeasylearn <theeasylearn@gmail.com>
2025-11-27 12:06:07 +0530

    index.js

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import './App.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+var num1 = 10;
+var num2 = 3;
+var division = num1 / num2
+var output = (<div>
+        <h1 className='myHeading'>using JSX in react js</h1>
+        <hr/>
+            <h2>using expression</h2>
+            <ul>
+                <li>Addition {num1+num2}</li>
+                <li>Subtraction {num1 - num2}</li>
+                <li>multiplication {num1 * num2}</li>
+                <li>Division {division}</li>
+                <li>relational expression (num1 above num2) {(num1>num2)?"num1 is bigger":"num1 is smaller"}</li>
+            </ul>    
+        </div>
+        ) 
+// jsx 
+root.render(output);
