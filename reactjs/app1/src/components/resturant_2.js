7afabb840a53364d3b4602aafd08df9e7e74de7e
theeasylearn <theeasylearn@gmail.com>
2025-12-12 15:16:27 +0530

    dinning tables

---
+++ reactjs/app1/src/components/resturant_2.js
@@ -0,0 +1,17 @@
+import React from 'react';  
+import DinningTable from './dinning_table_2';
+export default class Resturant extends React.Component {
+    render() {
+        return (<div className="container">
+            <div className="row">
+                {/* when you create Dinning table class, first constructor executes then render method execute */}
+                <DinningTable name='Ram Patel' tableno='9' />
+                <DinningTable name='Krishna pandya' tableno='25' />
+                {/* 
+                <DinningTable name='Mohan sharma' tableno='36' /> 
+                <DinningTable  tableno='99' name='Ankit Patel' />  */}
+            </div>
+        </div>
+        );
+    }
+}
