a0dafd2a78e35dc647c69702734bdd37da910f99
theeasylearn <theeasylearn@gmail.com>
2025-12-25 12:37:38 +0530

    conditional rendersing

---
+++ reactjs/app1/src/15_condtional_rendering_1.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+class MenuLinks extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false
+        };
+    }
+    //create arrow function to update state vaeriable
+    updateLogin = () => {
+        this.setState({
+            isLoggedIn: !this.state.isLoggedIn
+        });
+    }
+    GuestLink = () => {
+        if (this.state.isLoggedIn === false) {
+            return (<>
+
+
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Register</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#" onClick={this.updateLogin}>Login</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Forgot password</a>
+                </li>
+            </>);
+        }
+    }
+    UserLink = () => {
+        if (this.state.isLoggedIn === true) {
+            return (<>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Cart</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Checkout</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Change password</a>
+                </li>
+
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#"
+                        onClick={this.updateLogin}>Logout</a>
+                </li>
+            </>)
+        }
+    }
+    render() {
+        return (<div className="collapse navbar-collapse show" id="navbarBasic">
+            <ul className="navbar-nav me-auto mb-2 mb-xl-0">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Shop</a>
+                </li>
+                <this.GuestLink />
+                <this.UserLink />
+            </ul>
+
+        </div>)
+    }
+}
+function Menu() {
+    return (<>
+        <nav className="navbar navbar-expand-xl navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">the easylearn academy</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBasic" aria-controls="navbarBasic" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                {/* use MenuLink class as Tag */}
+                <MenuLinks />
+
+            </div>
+        </nav>
+    </>)
+}
+root.render(<Menu />);
--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,85 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-class MenuLinks extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoggedIn: false
-        };
-    }
-    //create arrow function to update state vaeriable
-    updateLogin = () => {
-        this.setState({
-            isLoggedIn: !this.state.isLoggedIn
-        });
-    }
-    GuestLink = () => {
-        if (this.state.isLoggedIn === false) {
-            return (<>
-
-
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Register</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#" onClick={this.updateLogin}>Login</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Forgot password</a>
-                </li>
-            </>);
-        }
-    }
-    UserLink = () => {
-        if (this.state.isLoggedIn === true) {
-            return (<>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Cart</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Checkout</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Change password</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#"
-                        onClick={this.updateLogin}>Logout</a>
-                </li>
-            </>)
-        }
-    }
-    render() {
-        return (<div className="collapse navbar-collapse show" id="navbarBasic">
-            <ul className="navbar-nav me-auto mb-2 mb-xl-0">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Shop</a>
-                </li>
-                <this.GuestLink />
-                <this.UserLink />
-            </ul>
-
-        </div>)
-    }
-}
-function Menu() {
-    return (<>
-        <nav className="navbar navbar-expand-xl navbar-light bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">the easylearn academy</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBasic" aria-controls="navbarBasic" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
-                </button>
-                {/* use MenuLink class as Tag */}
-                <MenuLinks />
-
+function FoodMenu(props) {
+    var output = props.items.map((current) => {
+        return <li className="list-group-item list-group-item-success">{current}</li>
+    });
+    return (<div className="container mt-5">
+        <div className="row">
+            <div className="col-12">
+                <h2>Menu Items</h2>
             </div>
-        </nav>
-    </>)
+        </div>
+        <div className="row">
+            <div className="col-lg-3">
+                <div className="card">
+                    <div className="card-body">
+                        <ol className="list-group list-group-numbered">
+                            {output}       
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
 }
-root.render(<Menu />);
+const items = ["Paneer Butter Masala","Shahi Paneer","Palak Paneer","Chole Bhature","Rajma Chawal","Dal Makhani","Aloo Paratha","Gobi Paratha","Masala Dosa","Idli Sambhar","Vegetable Biryani","Pav Bhaji","Veg Pulao","Kadhi Pakora","Baingan Bharta","Malai Kofta","Veg Korma","Sambar","Upma","Poha"];
+
+root.render(<FoodMenu items={items} />)
