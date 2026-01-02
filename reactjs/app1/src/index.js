59473084ec6a646f90556d05d1da8ca3bf3be6f9
theeasylearn <theeasylearn@gmail.com>
2026-01-02 12:09:03 +0530

    packages

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,53 +1,82 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-class AreaCalculator extends Component {
+class Category extends React.Component {
     constructor(props) {
         super(props);
-        //create property variable
-        this.lendth = React.createRef();
-        this.width = React.createRef();
         this.state = {
-            area: ''
-        };
+            items: []
+        }
     }
-    calculateArea = (event) => {
-        event.preventDefault();
-        console.log(this.lendth.current.value);
-        console.log(this.width.current.value);
-        this.setState({
-            area: this.lendth.current.value * this.width.current.value
+    componentDidMount() {
+        //always call api inside componentDidMount3
+        let apiAddress = "https://theeasylearnacademy.com/shop/ws/category.php";
+        axios({
+            url: apiAddress,
+            method: 'get',
+            responsetype: 'json'
+        }).then((response) => {
+            //console.log(response);
+            //check if response has error 
+            let data = response.data;
+            let error = data[0]['error'];
+            if (error !== 'no') {
+                alert(error);
+            }
+            else {
+                let total = data[1]['total'];
+                if (total === 0)
+                    alert('no category found')
+                else {
+                    data.splice(0, 2); //delete 2 object from begining 
+                    this.setState({
+                        items: data
+                    });
+                }
+            }
+            console.log(error);
+        }).catch((error) => {
+            console.log(error);
         });
+
     }
     render() {
-        return (<div className="container mt-5">
+        return (<div className="container mt-4">
             <div className="row justify-content-center">
-                <div className="col-lg-6 col-md-8 col-sm-12">
+                <div className="col-lg-100">
                     <div className="card shadow">
-                        <div className="card-header bg-primary text-white text-center">
-                            <h4>Area Calculator</h4>
+                        <div className="card-header bg-primary text-white">
+                            <h5 className="mb-0">Content List</h5>
                         </div>
-                        <div className="card-body">
-                            <form method='post' onSubmit={this.calculateArea}>
-                                <div className="mb-3">
-                                    <label className="form-label">Length</label>
-                                    <input type="number" className="form-control" placeholder="Enter length" ref={this.lendth} />
-                                </div>
-                                <div className="mb-3">
-                                    <label className="form-label">Width</label>
-                                    <input type="number" className="form-control" placeholder="Enter width" ref={this.width} />
-                                </div>
-                                <div className="d-grid">
-                                    <button type="submit" className="btn btn-primary">
-                                        Calculate Area
-                                    </button>
-                                </div>
-                                <div className="mt-3 text-center">
-                                    <h5>Area: <span className="text-primary">{this.state.area}</span></h5>
-                                </div>
-                            </form>
+                        <div className="card-body p-0">
+                            <div className="table-responsive">
+                                <table className="table table-bordered table-striped mb-0 align-middle text-center">
+                                    <thead className="table-dark">
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Title</th>
+                                            <th>Photo</th>
+                                            <th>Is Live</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {this.state.items.map((row) => {
+                                            return (<tr>
+                                                <td>{row.id}</td>
+                                                <td>{row.title}</td>
+                                                <td>
+                                                    <img src={"https://theeasylearnacademy.com/shop/images/category/" + row.photo} className="img-fluid img-thumbnail" alt="photo" />
+                                                </td>
+                                                <td>
+                                                    {row.islive === '1'?"Yes":"No"}
+                                                </td>
+                                            </tr>)
+                                        })};
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-
                     </div>
                 </div>
             </div>
@@ -55,4 +84,4 @@ class AreaCalculator extends Component {
         );
     }
 }
-root.render(<AreaCalculator />)
+root.render(<Category />)
